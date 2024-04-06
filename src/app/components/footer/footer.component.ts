385d180c871feac53c4f4cdd3c5ad4b8ddab0525
angel.petrushevski <angel.petrushevski@sorsix.com>
2024-04-06 22:19:08 +0800

    add instagram page

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -12,6 +12,7 @@ export class FooterComponent {
     { name: 'YouTube', icon: 'bi bi-youtube', url: 'https://www.youtube.com/channel/UCTztU8FSqIj0mN9XrRhJbhw' },
     { name: 'Meetup', icon: 'bi bi-calendar-event', url: 'https://www.meetup.com/angular-macedonia/' },
     { name: 'GitHub', icon: 'bi bi-github', url: 'https://github.com/Angular-Macedonia' },
-    { name: 'LinkedIn', icon: 'bi bi-linkedin', url: 'https://www.linkedin.com/company/angular-macedonia/' }
+    { name: 'LinkedIn', icon: 'bi bi-linkedin', url: 'https://www.linkedin.com/company/angular-macedonia/' },
+    { name: 'Instagram', icon: 'bi bi-instagram', url: 'https://www.instagram.com/angularmacedonia/' },
   ];
 }
